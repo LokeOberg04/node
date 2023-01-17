855dee3bc092e239301d47154d8b988f218b1cba
pppog <loke.oberg@elev.ga.ntig.se>
2023-01-17 14:49:05 +0100

    fixade hamburgar meny + sass + cssReset + innehåll

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const indexRouter = require('./routes/index');
+const createError = require('http-errors')
 const nunjucks = require('nunjucks');
 const express = require('express');
-const path = require('path')
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.use('/', indexRouter);
+
+app.use(express.static('public'))
 
 nunjucks.configure('views', {
     autoescape: true,
     express: app,
 });
 
-app.use('/', indexRouter);
-app.use(express.static('public'))
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.njk');
+});
+
+
